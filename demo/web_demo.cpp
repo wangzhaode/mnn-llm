68dee0ef57190fb174bab2eaeb6d000598b76b9b
雁行 <zhaode.wzd@alibaba-inc.com>
2023-12-15 17:48:40 +0800

    refactor web demo.

--- demo/web_demo.cpp
+++ demo/web_demo.cpp
@@ -13,7 +13,7 @@
 int main(int argc, const char* argv[]) {
     if (argc < 3) {
         std::cout << "Usage: " << argv[0] << " model_dir web_dir" << std::endl;
-        std::cout << "Example: " << argv[0] << " ../qwen-1.8b-int4 ../resource/web" << std::endl;
+        std::cout << "Example: " << argv[0] << " ../qwen-1.8b-int4 ../web" << std::endl;
         return 0;
     }
     std::string model_dir = argv[1];
@@ -29,9 +29,8 @@ int main(int argc, const char* argv[]) {
     std::string last_request = "";
     auto chat = [&](std::string str) {
         waiting = true;
-        llm->response(str, &ss);
+        llm->response(str, &ss, "<eop>");
         std::cout << "### response : " << ss.str() << std::endl;
-        ss << "<eop>";
         waiting = false;
     };
     svr.Post("/chat", [&](const httplib::Request &req, httplib::Response &res) {
@@ -50,9 +49,9 @@ int main(int argc, const char* argv[]) {
         }
     });
     svr.set_mount_point("/", web_dir);
-    printf(">>> please open http://0.0.0.0:5088\n");
+    printf(">>> please open http://0.0.0.0:8080 or http://localhost:8080\n");
     fflush(stdout);
-    svr.listen("0.0.0.0", 5088);
+    svr.listen("0.0.0.0", 8080);
     printf(">>> end\n");
     return 0;
 }

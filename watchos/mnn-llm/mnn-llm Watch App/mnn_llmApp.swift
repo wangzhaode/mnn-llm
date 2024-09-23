03c41a1c8aa6d7b6f712b35d57667f3fc2bf40d0
yanxing <zhaode.wzd@alibaba-inc.com>
2024-09-23 21:56:31 +0800

    [update] add watchos support.

---
+++ watchos/mnn-llm/mnn-llm Watch App/mnn_llmApp.swift
@@ -0,0 +1,17 @@
+//
+//  mnn_llmApp.swift
+//  mnn-llm Watch App
+//
+//  Created by 王召德 on 2024/9/23.
+//
+
+import SwiftUI
+
+@main
+struct mnn_llm_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ChatView()
+        }
+    }
+}
